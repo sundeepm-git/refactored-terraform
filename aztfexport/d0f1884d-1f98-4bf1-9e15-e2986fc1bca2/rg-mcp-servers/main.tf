4dec55ef78dc027e43f8aa5e78c734b18395b2a8
Sundeep Maheshwari <86803674+sundeepm-git@users.noreply.github.com>
2026-02-06 17:00:22 -0600

    Export Terraform for rg-mcp-servers: main.tf

--- aztfexport/d0f1884d-1f98-4bf1-9e15-e2986fc1bca2/rg-mcp-servers/main.tf
+++ aztfexport/d0f1884d-1f98-4bf1-9e15-e2986fc1bca2/rg-mcp-servers/main.tf
@@ -5,14 +5,14 @@
 #   Main Terraform configuration containing all Azure resource definitions exported from the resource group.
 #
 # GENERATED:
-#   Date: 2026-02-06 12:33:12
+#   Date: 2026-02-06 16:59:52
 #   By: sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221)
 #   Tool: Azure to Terraform Export (aztfexport)
 #
 # MODIFICATION HISTORY:
 #   Date       | Modified By | Description
 #   -----------|-------------|--------------------------------------------
-#   2026-02-06 12:33:12 | sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221) | Initial export from Azure
+#   2026-02-06 16:59:52 | sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221) | Initial export from Azure
 #
 # NOTES:
 #   - This file was automatically generated from existing Azure resources
@@ -26,5 +26,66 @@ resource "azurerm_storage_account" "res-0" {
   location                        = "eastus"
   name                            = "samcpstorage"
   resource_group_name             = "rg-mcp-servers"
+}
+
+
+terraform {
+  backend "local" {}
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.58.0"
+
+    }
+  }
+}
+
+
+terraform {
+  backend "local" {}
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.58.0"
+
+    }
+  }
+}
+
+
+resource "azurerm_container_registry" "res-0" {
+  admin_enabled       = true
+  location            = "centralus"
+  name                = "aztfmcpacr"
+  resource_group_name = "rg-mcp-servers"
+  sku                 = "Basic"
+}
+
+
+terraform {
+  backend "local" {}
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.58.0"
+
+    }
+  }
+}
+
+
+terraform {
+  backend "local" {}
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.58.0"
+
+    }
+  }
 }
 
