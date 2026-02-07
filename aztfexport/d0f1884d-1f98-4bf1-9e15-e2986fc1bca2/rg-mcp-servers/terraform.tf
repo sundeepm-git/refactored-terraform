74bdeac38402ec43f43ccc3ddca4e15463d3e8e7
Sundeep Maheshwari <86803674+sundeepm-git@users.noreply.github.com>
2026-02-06 20:06:28 -0600

    Export Terraform for rg-mcp-servers: terraform.tf

--- aztfexport/d0f1884d-1f98-4bf1-9e15-e2986fc1bca2/rg-mcp-servers/terraform.tf
+++ aztfexport/d0f1884d-1f98-4bf1-9e15-e2986fc1bca2/rg-mcp-servers/terraform.tf
@@ -5,14 +5,14 @@
 #   Terraform backend and required provider version constraints.
 #
 # GENERATED:
-#   Date: 2026-02-06 20:00:04
+#   Date: 2026-02-06 20:05:46
 #   By: sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221)
 #   Tool: Azure to Terraform Export (aztfexport)
 #
 # MODIFICATION HISTORY:
 #   Date       | Modified By | Description
 #   -----------|-------------|--------------------------------------------
-#   2026-02-06 20:00:04 | sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221) | Initial export from Azure
+#   2026-02-06 20:05:46 | sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221) | Initial export from Azure
 #
 # NOTES:
 #   - This file was automatically generated from existing Azure resources
