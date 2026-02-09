8b8ee5fc34c4076d485ab511c57a9be6b7f116b6
Sundeep Maheshwari <86803674+sundeepm-git@users.noreply.github.com>
2026-02-09 15:25:30 -0600

    Export Terraform for rg-mcp-servers: provider.tf

--- aztfexport/d0f1884d-1f98-4bf1-9e15-e2986fc1bca2/rg-mcp-servers/provider.tf
+++ aztfexport/d0f1884d-1f98-4bf1-9e15-e2986fc1bca2/rg-mcp-servers/provider.tf
@@ -5,14 +5,14 @@
 #   Azure provider configuration and authentication settings for Terraform.
 #
 # GENERATED:
-#   Date: 2026-02-09 14:48:42
+#   Date: 2026-02-09 15:24:52
 #   By: sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221)
 #   Tool: Azure to Terraform Export (aztfexport)
 #
 # MODIFICATION HISTORY:
 #   Date       | Modified By | Description
 #   -----------|-------------|--------------------------------------------
-#   2026-02-09 14:48:42 | sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221) | Initial export from Azure
+#   2026-02-09 15:24:52 | sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221) | Initial export from Azure
 #
 # NOTES:
 #   - This file was automatically generated from existing Azure resources
