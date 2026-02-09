fd7a1cdcbdab947e5c62e1c76f9a9a8d637f7326
Sundeep Maheshwari <86803674+sundeepm-git@users.noreply.github.com>
2026-02-08 21:46:08 -0600

    Export Terraform for rg-mcp-servers: main.tf

--- aztfexport/d0f1884d-1f98-4bf1-9e15-e2986fc1bca2/rg-mcp-servers/main.tf
+++ aztfexport/d0f1884d-1f98-4bf1-9e15-e2986fc1bca2/rg-mcp-servers/main.tf
@@ -5,14 +5,14 @@
 #   Main Terraform configuration containing all Azure resource definitions exported from the resource group.
 #
 # GENERATED:
-#   Date: 2026-02-06 16:59:52
+#   Date: 2026-02-08 21:44:43
 #   By: sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221)
 #   Tool: Azure to Terraform Export (aztfexport)
 #
 # MODIFICATION HISTORY:
 #   Date       | Modified By | Description
 #   -----------|-------------|--------------------------------------------
-#   2026-02-06 16:59:52 | sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221) | Initial export from Azure
+#   2026-02-08 21:44:43 | sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221) | Initial export from Azure
 #
 # NOTES:
 #   - This file was automatically generated from existing Azure resources
@@ -29,63 +29,16 @@ resource "azurerm_storage_account" "res-0" {
 }
 
 
-terraform {
-  backend "local" {}
-
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.58.0"
-
-    }
+# Source: main.aztfexport.tf
+resource "azurerm_container_app_environment" "res-0" {
+  location                   = "centralus"
+  log_analytics_workspace_id = data.azurerm_log_analytics_workspace.workspace_rgmcpserversIh7a.id
+  name                       = "mcp-aca-env"
+  resource_group_name        = "rg-mcp-servers"
+  workload_profile {
+    name                  = "Consumption"
+    workload_profile_type = "Consumption"
   }
 }
 
-
-terraform {
-  backend "local" {}
-
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.58.0"
-
-    }
-  }
-}
-
-
-resource "azurerm_container_registry" "res-0" {
-  admin_enabled       = true
-  location            = "centralus"
-  name                = "aztfmcpacr"
-  resource_group_name = "rg-mcp-servers"
-  sku                 = "Basic"
-}
-
-
-terraform {
-  backend "local" {}
-
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.58.0"
-
-    }
-  }
-}
-
-
-terraform {
-  backend "local" {}
-
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.58.0"
-
-    }
-  }
-}
 
