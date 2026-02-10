c1bcf6e7e84356600c2cb3698caef8e3fe4ff6a1
Sundeep Maheshwari <86803674+sundeepm-git@users.noreply.github.com>
2026-02-10 15:05:43 -0600

    Export Terraform for rg-mcp-servers: outputs.tf

--- aztfexport/d0f1884d-1f98-4bf1-9e15-e2986fc1bca2/rg-mcp-servers/outputs.tf
+++ aztfexport/d0f1884d-1f98-4bf1-9e15-e2986fc1bca2/rg-mcp-servers/outputs.tf
@@ -5,14 +5,14 @@
 #   Output value definitions to expose resource attributes after deployment.
 #
 # GENERATED:
-#   Date: 2026-02-10 14:43:10
+#   Date: 2026-02-10 15:04:52
 #   By: sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221)
 #   Tool: Azure to Terraform Export (aztfexport)
 #
 # MODIFICATION HISTORY:
 #   Date       | Modified By | Description
 #   -----------|-------------|--------------------------------------------
-#   2026-02-10 14:43:10 | sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221) | Initial export from Azure
+#   2026-02-10 15:04:52 | sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221) | Initial export from Azure
 #
 # NOTES:
 #   - This file was automatically generated from existing Azure resources
