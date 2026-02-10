eaea2e3de08aeb147a1a97299526c15e952541b8
Sundeep Maheshwari <86803674+sundeepm-git@users.noreply.github.com>
2026-02-09 22:50:36 -0600

    Export Terraform for rg-mcp-servers: variables.tf

--- aztfexport/d0f1884d-1f98-4bf1-9e15-e2986fc1bca2/rg-mcp-servers/variables.tf
+++ aztfexport/d0f1884d-1f98-4bf1-9e15-e2986fc1bca2/rg-mcp-servers/variables.tf
@@ -5,14 +5,14 @@
 #   Input variable definitions for parameterizing the Terraform configuration.
 #
 # GENERATED:
-#   Date: 2026-02-09 18:02:15
+#   Date: 2026-02-09 22:49:39
 #   By: sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221)
 #   Tool: Azure to Terraform Export (aztfexport)
 #
 # MODIFICATION HISTORY:
 #   Date       | Modified By | Description
 #   -----------|-------------|--------------------------------------------
-#   2026-02-09 18:02:15 | sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221) | Initial export from Azure
+#   2026-02-09 22:49:39 | sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221) | Initial export from Azure
 #
 # NOTES:
 #   - This file was automatically generated from existing Azure resources
@@ -20,6 +20,11 @@
 #   - Sensitive values may need to be parameterized
 # ==============================================================================
 # Variables
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
 variable "location" {
   description = "Azure region for resources"
   type        = string
@@ -29,5 +34,17 @@ variable "location" {
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "tags" {
+  description = "Common tags for all resources"
+  type        = map(string)
+  default     = {}
 }
 
