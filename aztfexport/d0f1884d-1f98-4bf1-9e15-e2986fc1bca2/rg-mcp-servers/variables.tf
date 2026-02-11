ae452f0a6f3e8c827b3f8b090169d4da495e406b
Sundeep Maheshwari <86803674+sundeepm-git@users.noreply.github.com>
2026-02-10 20:47:48 -0600

    Export Terraform for rg-mcp-servers: variables.tf

--- aztfexport/d0f1884d-1f98-4bf1-9e15-e2986fc1bca2/rg-mcp-servers/variables.tf
+++ aztfexport/d0f1884d-1f98-4bf1-9e15-e2986fc1bca2/rg-mcp-servers/variables.tf
@@ -5,14 +5,14 @@
 #   Input variable definitions for parameterizing the Terraform configuration.
 #
 # GENERATED:
-#   Date: 2026-02-10 15:04:56
+#   Date: 2026-02-10 20:46:59
 #   By: sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221)
 #   Tool: Azure to Terraform Export (aztfexport)
 #
 # MODIFICATION HISTORY:
 #   Date       | Modified By | Description
 #   -----------|-------------|--------------------------------------------
-#   2026-02-10 15:04:56 | sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221) | Initial export from Azure
+#   2026-02-10 20:46:59 | sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221) | Initial export from Azure
 #
 # NOTES:
 #   - This file was automatically generated from existing Azure resources
