3ccdf99d3e1f7159e7fd563f475bb89a57424f97
Sundeep Maheshwari <86803674+sundeepm-git@users.noreply.github.com>
2026-02-10 20:47:42 -0600

    Export Terraform for rg-mcp-servers: data-sources.tf

--- aztfexport/d0f1884d-1f98-4bf1-9e15-e2986fc1bca2/rg-mcp-servers/data-sources.tf
+++ aztfexport/d0f1884d-1f98-4bf1-9e15-e2986fc1bca2/rg-mcp-servers/data-sources.tf
@@ -5,14 +5,14 @@
 #   Data sources for external Azure resources referenced by this configuration. These resources are managed outside this Terraform state and are referenced as read-only.
 #
 # GENERATED:
-#   Date: 2026-02-10 15:04:43
+#   Date: 2026-02-10 20:46:44
 #   By: sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221)
 #   Tool: Azure to Terraform Export (aztfexport)
 #
 # MODIFICATION HISTORY:
 #   Date       | Modified By | Description
 #   -----------|-------------|--------------------------------------------
-#   2026-02-10 15:04:43 | sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221) | Initial export from Azure
+#   2026-02-10 20:46:44 | sunsu (4a7f6b45-8322-4cfe-bd16-008afdcc1221) | Initial export from Azure
 #
 # NOTES:
 #   - This file was automatically generated from existing Azure resources
